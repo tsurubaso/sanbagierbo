37855e225882540c3486a8e5e24a28850f78e144
tsurubaso <tsurubaso@hotmail.com>
2025-09-30 21:55:20 +0900

    prépa pour travailler sur Github api

---
+++ app/page.vcomparateur de text.page.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import InteractiveMerge from "@/components/DiffViewer";
+
+export default function ComparePage() {
+  const [original, setOriginal] = useState("");
+  const [modified, setModified] = useState("");
+
+  useEffect(() => {
+    async function loadFiles() {
+      const v1 = await fetch("/books/BaseE.md").then((res) => res.text());
+      const v2 = await fetch("/books/BaseE2.md").then((res) => res.text());
+      setOriginal(v1);
+      setModified(v2);
+    }
+    loadFiles();
+  }, []);
+
+  
+
+  return (
+    <div>
+      <h1>Fusion interactive</h1>
+      {original && modified ? (
+        <InteractiveMerge original={original} modified={modified} />
+      ) : (
+        <p>Chargement…</p>
+      )}
+    </div>
+  );
+}
